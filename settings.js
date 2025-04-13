0c9ebe444fc377f34a44eb76ae99d22b285237e9
sjsjajajajajaj <jajajajakkaksjhsshss@gmail.com>
2025-04-13 11:21:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID || "LAKA-MD=RpxR2CLT#VGHodbhWN_ojtunwTp4T3KEHxJK2O_IUp6E99xve_xk",
+SESSION_ID: process.env.SESSION_ID || "LAKA-MD=kxBETQwB#eTgqoOxgRq5YKuULA8twHW49ozuCx9V1Waq5tHRQosk",
 PREFIX: process.env.PREFIX || '.' ,
 POSTGRESQL_URL: process.env.POSTGRESQL_URL === undefined ? 'postgresql://postgres:lakamd@db.ndduyrkebubwlbwiljee.supabase.co:5432/postgres' : process.env.POSTGRESQL_URL
 };
